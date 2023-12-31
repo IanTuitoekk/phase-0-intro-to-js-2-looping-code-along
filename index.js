976ff8e972792d42b2b35f0ce65c5c4959ef4356
IanTuitoek <iantuitoek4@gmail.com>
2023-12-31 16:35:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+const name=["John", "Jane", "Ashley"]
+function writeCards(name){
+const thankyouCards=[];
+    for (let i = 0;i<name.length; i++){
+    thankyouCards.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+    }
+    return thankyouCards;
+}
+function countDown(number){
+    while(number>-1){
+        console.log(number);
+        number--;
+    }
+}
